3b462b9cb9f4563e8634dc3969ca1f094cd5096a
ashish210391 <ashishkumarb32@gmail.com>
2016-06-29 15:39:58 +0530

    bootstrap

--- mywebsite/includes/js/script.js
+++ mywebsite/includes/js/script.js
@@ -15,4 +15,9 @@ $(function(){
     e.preventDefault();
     $('#successAlert').slideDown();
   });
+  $('a.pop').click(function(e){
+    e.preventDefault();
+  });
+  $('a.pop').popover();
+  $('[rel="tooltip"]').tooltip();
 });
